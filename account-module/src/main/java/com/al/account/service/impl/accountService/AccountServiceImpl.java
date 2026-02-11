2e727dd7f7bcd096c850d40ca297c9ea76b6bf3c
zhangqin <zhangqin@umpay.com>
2026-02-11 16:33:41 +0800

    提交README.md

--- account-module/src/main/java/com/al/account/service/impl/accountService/AccountServiceImpl.java
+++ account-module/src/main/java/com/al/account/service/impl/accountService/AccountServiceImpl.java
@@ -18,10 +18,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Objects;
