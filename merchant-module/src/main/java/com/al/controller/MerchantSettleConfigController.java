d5926717cfd1aa7a597ead2ec01d3b9ad9dbc857
zhangqin <zhangqin@umpay.com>
2026-03-08 19:18:09 +0800

    增加商户结算信息配置

---
+++ merchant-module/src/main/java/com/al/controller/MerchantSettleConfigController.java
@@ -0,0 +1,27 @@
+package com.al.controller;
+
+import com.al.bean.dto.MerchantSettleBindDTO;
+import com.al.common.result.Result;
+import com.al.service.MerchantSettleConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/settle")
+public class MerchantSettleConfigController {
+    @Autowired
+    private MerchantSettleConfigService merchantSettleConfigService;
+    @PostMapping("/insert")
+    public Result insert(MerchantSettleBindDTO merchantSettleBindDTO) {
+        return Result.success(merchantSettleConfigService.bindAccount(merchantSettleBindDTO));
+    }
+    @GetMapping("/query")
+    public Result queryByMerchant(@RequestParam String merchantNo, @RequestParam String busiType ) {
+        return Result.success(merchantSettleConfigService.queryByMerchant(merchantNo,busiType));
+    }
+    @GetMapping("/update")
+    public Result update(@RequestParam String merchantNo, @RequestParam String busiType ) {
+        return Result.success(merchantSettleConfigService.discard(merchantNo,busiType));
+    }
+
+}
