8d8ba10dce59515e7feff87bb8f8fd8f47c206b1
zhangqin <zhangqin@umpay.com>
2026-06-19 17:20:46 +0800

    Add reconcile module and include Groovy dependency in pom.xml

---
+++ billing-module/src/main/java/com/al/billing/mapper/BillingTemplateTierMapper.java
@@ -0,0 +1,9 @@
+package com.al.billing.mapper;
+
+import com.al.billing.bean.vo.BillingTemplateTierVo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BillingTemplateTierMapper extends BaseMapper<BillingTemplateTierVo> {
+}
