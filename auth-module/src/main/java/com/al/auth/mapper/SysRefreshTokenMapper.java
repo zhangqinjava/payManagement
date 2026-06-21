6a56478de0dbd90059973a436b735f3f5164d761
zhangqin <zhangqin@umpay.com>
2026-06-21 18:19:30 +0800

    Refactor authentication logic in auth-module to enhance security and user management. Improved JWT integration and optimized related services for better performance.

---
+++ auth-module/src/main/java/com/al/auth/mapper/SysRefreshTokenMapper.java
@@ -0,0 +1,9 @@
+package com.al.auth.mapper;
+
+import com.al.auth.bean.vo.SysRefreshTokenVo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SysRefreshTokenMapper extends BaseMapper<SysRefreshTokenVo> {
+}
