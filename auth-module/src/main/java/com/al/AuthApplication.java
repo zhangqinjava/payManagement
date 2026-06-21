dee89010b6d98eae9fad6211295b569a209d6fe0
zhangqin <zhangqin@umpay.com>
2026-06-21 18:17:51 +0800

    Refactor authentication logic in auth-module to enhance security and user management. Improved integration with JWT functionality and updated related services for better performance.

---
+++ auth-module/src/main/java/com/al/AuthApplication.java
@@ -0,0 +1,13 @@
+package com.al;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.al.auth.mapper")
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
